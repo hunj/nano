979703aa3d6f52ba7dbd95fb458b450bc56bbd78
Hun Jae Lee <hunjael@gmail.com>
2021-03-06 09:12:27 -0500

    :sparkles: use cogs to be able to un/load on the fly

--- hakase/client.py
+++ hakase/client.py
@@ -1,11 +1,44 @@
 import discord
+from discord.ext import commands
+from discord.ext.commands import Context
 
-class HakaseClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+import os
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
 
-client = MyClient()
-client.run('my token goes here')
+token_file_path = os.environ.get("DISCORD_BOT_TOKEN")
+with open(token_file_path, 'r') as token_file:
+    TOKEN = token_file.read()
+
+client = commands.Bot(command_prefix='.')
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game("Listening to .help"))
+    print("I am online")
+
+
+@client.command()
+async def load(ctx: Context, extension):
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send("Loaded Cog")
+
+
+@client.command()
+async def unload(ctx: Context, extension):
+    client.unload_extension(f'cogs.{extension}')
+    await ctx.send("Unloaded Cog")
+
+
+@client.command()
+async def reload(ctx: Context, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send("Reloaded Cog")
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[0:-3]}')
+
+
+client.run(TOKEN)
