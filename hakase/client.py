ac5b5de58e1d2a07f2eecb9693f44e8c22e74eef
Hun Jae Lee <hunjael@gmail.com>
2021-03-06 11:05:59 -0500

    :gear: shorten hash & set activity to streaming with url

--- hakase/client.py
+++ hakase/client.py
@@ -24,7 +24,7 @@ client = commands.Bot(command_prefix=commands.when_mentioned)
 
 @client.event
 async def on_ready():
-    activity = discord.Activity(type=discord.ActivityType.listening, name=f"on {COMMIT_HASH}")
+    activity = discord.Activity(type=discord.ActivityType.streaming, name=f"on {COMMIT_HASH[:8]}", url="https://github.com/hunj/hakase")
     await client.change_presence(status=discord.Status.idle, activity=activity)
 
 
