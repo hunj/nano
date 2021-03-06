9addbf485d3a28b7de94fed3f5865d9f6ed13d5e
Hun Jae Lee <hunjael@gmail.com>
2021-03-06 11:54:00 -0500

    :sparkles: print ready msg to help debugging

--- hakase/client.py
+++ hakase/client.py
@@ -24,6 +24,7 @@ client = commands.Bot(command_prefix=commands.when_mentioned)
 
 @client.event
 async def on_ready():
+    print("I'm ready")
     activity = discord.Activity(type=discord.ActivityType.listening, name=COMMIT_HASH[:8])
     await client.change_presence(status=discord.Status.idle, activity=activity)
 
