dab25b41cf731fd21682c7b6d315f60c7b97b035
Hun Jae Lee <hunjael@gmail.com>
2021-03-06 08:30:43 -0500

    :sparkles: initial commit; basic file structure

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.9.2
+ENV PYTHONUNBUFFERED 1
+
+RUN mkdir /app
+WORKDIR /app
+
+COPY requirements.txt /app
+RUN pip install -r requirements.txt
+
+COPY hakase /app
---
+++ hakase/client.py
@@ -0,0 +1,11 @@
+import discord
+
+class HakaseClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run('my token goes here')
